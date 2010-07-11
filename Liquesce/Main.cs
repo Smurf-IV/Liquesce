ae2f9d998f27e31736422271f2cd5a9d1924c5be
smurfiv_cp <SND\smurfiv_cp>
2010-07-11 18:33:47 +0000

    Initial Creation of Service project - built only

---
+++ Liquesce/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace Liquesce
+{
+   public partial class Main : ServiceBase
+   {
+      public Main()
+      {
+         InitializeComponent();
+      }
+
+      protected override void OnStart(string[] args)
+      {
+      }
+
+      protected override void OnStop()
+      {
+      }
+   }
+}
