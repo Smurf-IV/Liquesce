579e7d89ef73c00595003ef58e6f5695f7546f6c
smurfiv_cp <SND\smurfiv_cp>
2010-07-22 19:43:17 +0000

    Add Version information plugin (YYYY-MM-DD based). Add in NLog and test it working. No Functionality yet !

---
+++ Liquesce/Liquesce/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Liquesce
+{
+   public partial class MainForm : Form
+   {
+      public MainForm()
+      {
+         InitializeComponent();
+      }
+   }
+}
