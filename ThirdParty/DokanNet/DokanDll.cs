aa7935cb52853f671b851cf84cd518461c0a8d80
smurfiv_cp <SND\smurfiv_cp>
2010-08-07 10:34:38 +0000

    Add in the 1st pass of the NLog additions to the Dokan.Net interface

---
+++ ThirdParty/DokanNet/DokanDll.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DokanNet
+{
+   [StructLayout(LayoutKind.Sequential, Pack = 4)]
+   struct DOKAN_OPTIONS
+   {
+      public char DriveLetter; // driver letter to be mounted
+      public ushort ThreadCount; // number of threads to be used
+      public uint Options;
+      private readonly ulong Dummy1;
+   }
+
+   // this struct must be the same layout as DOKAN_OPERATIONS
+   [StructLayout(LayoutKind.Sequential, Pack = 4)]
+   struct DOKAN_OPERATIONS
+   {
+      public Proxy.CreateFileDelegate CreateFile;
+      public Proxy.OpenDirectoryDelegate OpenDirectory;
+      public Proxy.CreateDirectoryDelegate CreateDirectory;
+      public Proxy.CleanupDelegate Cleanup;
+      public Proxy.CloseFileDelegate CloseFile;
+      public Proxy.ReadFileDelegate ReadFile;
+      public Proxy.WriteFileDelegate WriteFile;
+      public Proxy.FlushFileBuffersDelegate FlushFileBuffers;
+      public Proxy.GetFileInformationDelegate GetFileInformation;
+      public Proxy.FindFilesDelegate FindFiles;
+      private readonly IntPtr FindFilesWithPattern; // this is not used in DokanNet
+      public Proxy.SetFileAttributesDelegate SetFileAttributes;
+      public Proxy.SetFileTimeDelegate SetFileTime;
+      public Proxy.DeleteFileDelegate DeleteFile;
+      public Proxy.DeleteDirectoryDelegate DeleteDirectory;
+      public Proxy.MoveFileDelegate MoveFile;
+      public Proxy.SetEndOfFileDelegate SetEndOfFile;
+      public Proxy.SetAllocationSizeDelegate SetAllocationSize;
+      public Proxy.LockFileDelegate LockFile;
+      public Proxy.UnlockFileDelegate UnlockFile;
+      public Proxy.GetDiskFreeSpaceDelegate GetDiskFreeSpace;
+      public Proxy.GetVolumeInformationDelegate GetVolumeInformation;
+      public Proxy.UnmountDelegate Unmount;
+   }
+
+   static class DokanDll
+   {
+      [DllImport("dokan.dll")]
+      public static extern int DokanMain(ref DOKAN_OPTIONS options, ref DOKAN_OPERATIONS operations);
+
+      [DllImport("dokan.dll")]
+      public static extern int DokanUnmount(int driveLetter);
+
+      [DllImport("dokan.dll")]
+      public static extern uint DokanVersion();
+
+      [DllImport("dokan.dll")]
+      public static extern uint DokanDriveVersion();
+
+      [DllImport("dokan.dll")]
+      public static extern bool DokanResetTimeout(uint timeout, ref DOKAN_FILE_INFO rawFileInfo);
+   }
+}
--- ThirdParty/DokanNet/Properties/AssemblyInfo.cs
+++ ThirdParty/DokanNet/Properties/AssemblyInfo.cs
@@ -2,15 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+//
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LiquesceSvc")]
+//
+[assembly: AssemblyTitle("DokanNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Smurf-IV")]
-[assembly: AssemblyProduct("LiquesceSvc")]
-[assembly: AssemblyCopyright("Copyright © Smurf-IV 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DokanNet")]
+[assembly: AssemblyCopyright("Copyright (C)  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,9 +21,13 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f58ce85d-120a-4d4e-bde1-8e52ebd50b95")]
 
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("faf94eee-9bae-4ada-8f96-614ca17f7854")]
+
+
+//
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -29,8 +35,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2010.8.7.3")]
-[assembly: AssemblyFileVersion("2010.8.7.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
