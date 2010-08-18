330f2bf913b052bf6351e079bfcb45ddbc6ce878
smurfiv_cp <SND\smurfiv_cp>
2010-08-18 19:30:47 +0000

    Added in Thread protection for Adding and Removing Dir's / files under thread load

--- LiquesceFaçade/ConfigDetails.cs
+++ LiquesceFaçade/ConfigDetails.cs
@@ -11,11 +11,12 @@ namespace LiquesceFaçade
    public class ConfigDetails 
    {
       public uint DelayStartMilliSec;
-      public char DriveLetter;
+      public string DriveLetter; // Make this is a string so that the XML looks better (Rather than exporting 72 for 'N')
       public ushort ThreadCount;
+      public int LockTimeout = short.MaxValue; // Useful if you are getting locks in the multiple threads - Can be set to -1 for infinite
       public bool DebugMode;
       public string VolumeLabel;
-      public UInt64 HoldOffBufferBytes = 10L<<10<<10<<10; // ==10GB;
+      public UInt64 HoldOffBufferBytes = 1L<<10<<10<<10; // ==1GB;
       public UInt32 BufferReadSize = 4 << 10;   // == 4K Standard OS build block size
       public List<string> SourceLocations;
       // TODO: Extend to include encypted ACL's and things
