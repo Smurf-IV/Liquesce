6cdd8056751de98d793d13ca9de1b35c8b868146
smurfiv_cp <SND\smurfiv_cp>
2010-08-30 09:57:23 +0000

    - Fixed Meta<Browser> adding new Xml files over a share (Tested on XP and win 7) - Tested Copying to the network share (On Win 7) using Terracopy - Tested Create of a new directory on Win 7 share

--- Liquesce/LiquesceFaçade/Properties/AssemblyInfo.cs
+++ Liquesce/LiquesceFaçade/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2010.8.29.136")]
-[assembly: AssemblyFileVersion("2010.8.29.136")]
+[assembly: AssemblyVersion("2010.8.30.139")]
+[assembly: AssemblyFileVersion("2010.8.30.139")]
