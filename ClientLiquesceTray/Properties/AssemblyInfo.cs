b4dc42c69e4ae4b12f976269e0b175dab64ecd6d
smurfiv_cp <SND\smurfiv_cp>
2010-10-25 15:28:03 +0000

    - Ready the solution for the Client share side implementation

--- ClientLiquesceTray/Properties/AssemblyInfo.cs
+++ ClientLiquesceTray/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LiquesceFaçade")]
+[assembly: AssemblyTitle("ClientLiquesceTray")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TTV")]
-[assembly: AssemblyProduct("LiquesceFaçade")]
+[assembly: AssemblyProduct("ClientLiquesceTray")]
 [assembly: AssemblyCopyright("Copyright © TTV 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0add1bfe-80d9-460b-ab4f-1db837df49b3")]
+[assembly: Guid("379e5b37-042b-49a9-93b8-aa3f3bd8825d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2010.10.25.308")]
-[assembly: AssemblyFileVersion("2010.10.25.308")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
