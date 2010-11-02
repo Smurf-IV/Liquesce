3a50f9cca640eb354343b446c9877dc68dfcfc02
smurfiv_cp <SND\smurfiv_cp>
2010-11-02 21:16:57 +0000

    - Add startup and correct userID retrieval to Client - Possibly fix the resource usage when copying a load of files due to EnterUpgradeable documentation being a bit pants

--- ClientLiquesceSvc/ProcessIDToUser.cs
+++ ClientLiquesceSvc/ProcessIDToUser.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using System.Security;
 using System.Security.Principal;
-using System.Text;
+using System.Threading;
 using NLog;
 
 // Forward declarations
@@ -11,132 +12,13 @@ using HANDLE = System.IntPtr;
 
 namespace ClientLiquesceSvc
 {
-   // Code stolen from http://www.codeproject.com/KB/cs/processownersid.aspx
+   // Some code stolen from http://bytes.com/topic/c-sharp/answers/463942-using-openprocesstoken
    //
    internal static class ProcessIDToUser
    {
       static private readonly Logger Log = LogManager.GetCurrentClassLogger();
-      private const int TOKEN_QUERY = 0X00000008;
-
-      enum TOKEN_INFORMATION_CLASS
-      {
-         TokenUser = 1,
-         TokenGroups,
-         TokenPrivileges,
-         TokenOwner,
-         TokenPrimaryGroup,
-         TokenDefaultDacl,
-         TokenSource,
-         TokenType,
-         TokenImpersonationLevel,
-         TokenStatistics,
-         TokenRestrictedSids,
-         TokenSessionId
-      }
-
-
-      [StructLayout(LayoutKind.Sequential)]
-      struct TOKEN_USER
-      {
-         public _SID_AND_ATTRIBUTES User;
-      }
-
-      [StructLayout(LayoutKind.Sequential)]
-      private struct _SID_AND_ATTRIBUTES
-      {
-         public readonly IntPtr Sid;
-         private readonly int Attributes;
-      }
-
-      [DllImport("advapi32")]
-      static extern bool OpenProcessToken(
-          HANDLE ProcessHandle, // handle to process
-          int DesiredAccess, // desired access to process
-          ref IntPtr TokenHandle // handle to open access token
-      );
-
-      [DllImport("kernel32")]
-      static extern HANDLE GetCurrentProcess();
-
-      [DllImport("advapi32", CharSet = CharSet.Auto)]
-      static extern bool GetTokenInformation(
-          HANDLE hToken,
-          TOKEN_INFORMATION_CLASS tokenInfoClass,
-          IntPtr TokenInformation,
-          int tokeInfoLength,
-          ref int reqLength
-      );
-
-      [DllImport("kernel32")]
-      static extern bool CloseHandle(HANDLE handle);
-
-      [DllImport("advapi32", CharSet = CharSet.Auto)]
-      static extern bool ConvertSidToStringSid(
-          IntPtr pSID,
-          [In, Out, MarshalAs(UnmanagedType.LPTStr)] ref string pStringSid
-      );
-
-      [DllImport("advapi32", CharSet = CharSet.Auto)]
-      static extern bool ConvertStringSidToSid(
-          [In, MarshalAs(UnmanagedType.LPTStr)] string pStringSid,
-          ref IntPtr pSID
-      );
-
-      /// <summary>
-      /// Collect User Info
-      /// </summary>
-      /// <param name="pToken">Process Handle</param>
-      /// <param name="SID"></param>
-      private static bool GetUserSidPtr(HANDLE pToken, out IntPtr SID)
-      {
-         const int Access = TOKEN_QUERY;
-         HANDLE procToken = IntPtr.Zero;
-         bool ret = false;
-         SID = IntPtr.Zero;
-         try
-         {
-            if (OpenProcessToken(pToken, Access, ref procToken))
-            {
-               ret = ProcessTokenToSid(procToken, out SID);
-               CloseHandle(procToken);
-            }
-            return ret;
-         }
-         catch (Exception ex)
-         {
-            Log.WarnException("GetUserSidPtr", ex);
-            return false;
-         }
-      }
-
-      private static bool ProcessTokenToSid(HANDLE token, out IntPtr SID)
-      {
-         TOKEN_USER tokUser;
-         const int bufLength = 256;
-         IntPtr tu = Marshal.AllocHGlobal(bufLength);
-         SID = IntPtr.Zero;
-         try
-         {
-            int cb = bufLength;
-            bool ret = GetTokenInformation(token, TOKEN_INFORMATION_CLASS.TokenUser, tu, cb, ref cb);
-            if (ret)
-            {
-               tokUser = (TOKEN_USER)Marshal.PtrToStructure(tu, typeof(TOKEN_USER));
-               SID = tokUser.User.Sid;
-            }
-            return ret;
-         }
-         catch (Exception ex)
-         {
-            Log.WarnException("ProcessTokenToSid", ex);
-            return false;
-         }
-         finally
-         {
-            Marshal.FreeHGlobal(tu);
-         }
-      }
-
+      static private readonly Dictionary<string, string> quickLookup = new Dictionary<string, string>();
+      static private readonly ReaderWriterLockSlim quickLookupSync = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
 
       /// <summary>
       /// From the ProcessID, this will attempt to get the Domain and UserName that currently owns it
@@ -151,26 +33,50 @@ namespace ClientLiquesceSvc
          {
             Process process = Process.GetProcessById((int)PID);
             // Having had a go at trying to work out how to do this with out having to use the 
-            // wieght of the internal calls to get the processName, it appears that you still need a process.Handle to the Win32 API's
-            // The idea was to make a Map of process ID / App name to the user for quick lookup, as the ProcessID's can be recycled
-            // Also the internal Get### calls all use a processManager which seems to handle a lot of this in the cache;
+            // weight of the internal calls to get the processName, it appears that you still need a process.Handle to the Win32 API's
+            // Also the internal Get### calls all use a CLR .Net processManager which seems to handle a lot of this in the cache;
             // So speed will be the same unless this is forked out to a C++ / Win32 DLL to make the calls directly !
             processName = process.ProcessName;
-            ProcessStartInfo startInfo = process.StartInfo;
-            if (!String.IsNullOrEmpty(startInfo.Domain)
-               && !String.IsNullOrEmpty(startInfo.UserName)
-               )
+            string key = processName + PID;
+            try
             {
-               domainUser = String.Format(@"{0}\{1}", startInfo.Domain, startInfo.UserName);
-            }
-            else
-            {
-               IntPtr pSid;
-               if (GetUserSidPtr(process.Handle, out pSid))
+               quickLookupSync.EnterUpgradeableReadLock();
+               if (!quickLookup.TryGetValue(key, out domainUser))
                {
-                  // convert the user sid to a domain\name
-                  domainUser = new SecurityIdentifier(pSid).Translate(typeof (NTAccount)).ToString();
+                  quickLookupSync.EnterWriteLock();
+                  ProcessStartInfo startInfo = process.StartInfo;
+                  if (!String.IsNullOrEmpty(startInfo.Domain)
+                      && !String.IsNullOrEmpty(startInfo.UserName)
+                     )
+                  {
+                     domainUser = String.Format(@"{0}\{1}", startInfo.Domain, startInfo.UserName);
+                  }
+                  else
+                  {
+                     HANDLE hToken = new HANDLE();
+                     try
+                     {
+                        if (!OpenProcessToken(process.Handle, (int) TOKEN_READ, ref hToken))
+                           throw new ApplicationException("Could not get process token.  Win32 Error Code: " +
+                                                          Marshal.GetLastWin32Error());
+                        else
+                        {
+                           domainUser = new WindowsIdentity(hToken).Name;
+                        }
+                     }
+                     finally
+                     {
+                        CloseHandle(hToken);
+                     }
+                     quickLookup[key] = domainUser;
+                  }
                }
+            }
+            finally
+            {
+               if (quickLookupSync.IsWriteLockHeld)
+                  quickLookupSync.ExitWriteLock();
+               quickLookupSync.ExitUpgradeableReadLock();
             }
          }
          catch
@@ -181,5 +87,35 @@ namespace ClientLiquesceSvc
          Log.Trace("domainUser[{0}] found for PID[{1}] = {2}", domainUser, PID, processName);
          return domainUser;
       }
+
+      private const uint STANDARD_RIGHTS_REQUIRED = 0x000F0000;
+      private const uint STANDARD_RIGHTS_READ = 0x00020000;
+      private const uint TOKEN_ASSIGN_PRIMARY = 0x0001;
+      private const uint TOKEN_DUPLICATE = 0x0002;
+      private const uint TOKEN_IMPERSONATE = 0x0004;
+      private const uint TOKEN_QUERY = 0x0008;
+      private const uint TOKEN_QUERY_SOURCE = 0x0010;
+      private const uint TOKEN_ADJUST_PRIVILEGES = 0x0020;
+      private const uint TOKEN_ADJUST_GROUPS = 0x0040;
+      private const uint TOKEN_ADJUST_DEFAULT = 0x0080;
+      private const uint TOKEN_ADJUST_SESSIONID = 0x0100;
+      private const uint TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY);
+      private const uint TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
+          TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE |
+          TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT |
+          TOKEN_ADJUST_SESSIONID);
+
+      [DllImport("advapi32", SetLastError = true),
+      SuppressUnmanagedCodeSecurityAttribute]
+      static extern bool OpenProcessToken(
+          HANDLE ProcessHandle, // handle to process
+          int DesiredAccess, // desired access to process
+          ref IntPtr TokenHandle // handle to open access token
+      );
+
+      [DllImport("kernel32", SetLastError = true),
+      SuppressUnmanagedCodeSecurityAttribute]
+      static extern bool CloseHandle(HANDLE handle);
+
    }
 }
