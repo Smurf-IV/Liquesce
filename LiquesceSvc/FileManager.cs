f47765a956c04f44a74d0369b4ac3179a6582924
fpdragon_cp <SND\fpdragon_cp>
2010-11-06 09:32:54 +0000

    first steps to implement FindFilesWithPattern completely

--- LiquesceSvc/FileManager.cs
+++ LiquesceSvc/FileManager.cs
@@ -10,6 +10,12 @@ namespace LiquesceSvc
 {
     class FileManager
     {
+
+        private static const char DOS_STAR = '<';
+        private static const char DOS_QM = '>';
+        private static const char DOS_DOT = '"';
+
+
         private static Object lockvarMirrorToDo = "";
         //private static List<string> MirrorToDo = new List<string>();
         private static MirrorToDoList MirrorToDo = new MirrorToDoList();
@@ -44,6 +50,7 @@ namespace LiquesceSvc
         }
 
 
+
         // returns all elements of the to do list and removes all elements
         public static MirrorToDoList ConsumeMirrorToDo()
         {
@@ -57,6 +64,7 @@ namespace LiquesceSvc
         }
 
 
+
         public static void AddMirrorToDo(MirrorToDo entry)
         {
             lock (lockvarMirrorToDo)
@@ -66,11 +74,134 @@ namespace LiquesceSvc
         }
 
 
+
         public static string GetLocationFromFilePath(string path)
         {
             return path.Substring(0, path.LastIndexOf("\\"));
         }
 
+
+
+        // check whether Name matches Expression
+        // Expression can contain "?"(any one character) and "*" (any string)
+        // when IgnoreCase is TRUE, do case insenstive matching
+        public bool IsNameInExpression(string Expression, string Name, bool IgnoreCase)
+        {
+            int ei = 0;
+            int ni = 0;
+
+            while (ei < Expression.Length)
+            {
+
+                if (Expression[ei] == '*')
+                {
+                    ei++;
+                    if (Expression[ei] == '\0')
+                        return true;
+
+                    while (Name[ni] != '\0')
+                    {
+                        if (IsNameInExpression(Expression.Substring(ei), Name.Substring(ni), IgnoreCase))
+                            return true;
+                        ni++;
+                    }
+
+                }
+                else if (Expression[ei] == DOS_STAR)
+                {
+
+                    int p = ni + 1;
+                    int lastDot = 0;
+                    ei++;
+
+                    while (p < Name.Length)
+                    {
+                        if (Name[p] == '.')
+                            lastDot = p;
+                        p++;
+                    }
+
+
+                    while (true)
+                    {
+                        if (ni >= Name.Length || ni == lastDot)
+                            break;
+
+                        if (IsNameInExpression(Expression.Substring(ei), Name.Substring(ni), IgnoreCase))
+                            return true;
+                        ni++;
+                    }
+
+                }
+                else if (Expression[ei] == DOS_QM)
+                {
+
+                    ei++;
+                    if (Name[ni] != '.')
+                    {
+                        ni++;
+                    }
+                    else
+                    {
+
+                        int p = ni + 1;
+                        while (p < Name.Length)
+                        {
+                            if (Name[p] == '.')
+                                break;
+                            p++;
+                        }
+
+                        if (Name[p] == '.')
+                            ni++;
+                    }
+
+                }
+                else if (Expression[ei] == DOS_DOT)
+                {
+                    ei++;
+
+                    if (Name[ni] == '.')
+                        ni++;
+
+                }
+                else
+                {
+                    if (Expression[ei] == '?')
+                    {
+                        ei++; ni++;
+                    }
+                    else if (IgnoreCase && CompareChars(Expression[ei], Name[ni], true))
+                    {
+                        ei++; ni++;
+                    }
+                    else if (!IgnoreCase && Expression[ei] == Name[ni])
+                    {
+                        ei++; ni++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (ei == Expression.Length && ni == Name.Length)
+                return true;
+
+            return false;
+        }
+
+
+        private bool CompareChars(char char1, char char2, bool caseinsensitive)
+        {
+            if (caseinsensitive == false)
+                return (char1 == char2);
+            else
+            {
+                return char1.ToString().Equals(char2.ToString(), StringComparison.CurrentCultureIgnoreCase);
+            }
+        }
 
     }
 }
