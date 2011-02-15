c682a53622f9763644c8e90f5f7dfce44aa658e7
smurfiv_cp <SND\smurfiv_cp>
2011-02-15 19:20:33 +0000

    - Update ChangeLog

--- LiquesceSvc/Properties/AssemblyInfo.cs
+++ LiquesceSvc/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2011.1.12.620")]
-[assembly: AssemblyFileVersion("2011.1.12.620")]
+[assembly: AssemblyVersion("2011.2.15.622")]
+[assembly: AssemblyFileVersion("2011.2.15.622")]
