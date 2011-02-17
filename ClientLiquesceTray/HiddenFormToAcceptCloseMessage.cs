4caa45cad4629700fea61496c33ab398a20cdc17
smurfiv_cp <SND\smurfiv_cp>
2011-02-17 18:40:56 +0000

    Branch the code for Phase 1-> Phase II

---
+++ ClientLiquesceTray/HiddenFormToAcceptCloseMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClientLiquesceTray
+{
+   public partial class HiddenFormToAcceptCloseMessage : Form
+   {
+      public HiddenFormToAcceptCloseMessage()
+      {
+         InitializeComponent();
+      }
+
+      private void HiddenFormToAcceptCloseMessage_Load(object sender, EventArgs e)
+      {
+         Visible = false;
+      }
+
+      private void HiddenFormToAcceptCloseMessage_FormClosing(object sender, FormClosingEventArgs e)
+      {
+         Application.Exit();
+      }
+   }
+}
