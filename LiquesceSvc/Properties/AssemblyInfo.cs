ef37663944c3ed1d1ba79e186957cf4acb61b29f
smurfiv_cp <SND\smurfiv_cp>
2011-10-10 17:45:13 +0000

    - Add GNU header requirements to the cs files

--- LiquesceSvc/Properties/AssemblyInfo.cs
+++ LiquesceSvc/Properties/AssemblyInfo.cs
@@ -1,4 +1,29 @@
-﻿using System.Reflection;
+﻿#region Copyright (C)
+// ---------------------------------------------------------------------------------------------------------------
+//  <copyright file="AssemblyInfo.cs" company="Smurf-IV">
+// 
+//  Copyright (C) 2010-2011 Smurf-IV
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//   any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see http://www.gnu.org/licenses/.
+//  </copyright>
+//  <summary>
+//  Url: http://Liquesce.codeplex.com/
+//  Email: http://www.codeplex.com/site/users/view/smurfiv
+//  </summary>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -31,5 +56,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2011.10.4.682")]
-[assembly: AssemblyFileVersion("2011.10.4.682")]
+[assembly: AssemblyVersion("2011.10.10.683")]
+[assembly: AssemblyFileVersion("2011.10.10.683")]
