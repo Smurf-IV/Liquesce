cba716b7663683ef670cd1816df5a6b1b08902cf
smurfiv_cp <SND\smurfiv_cp>
2012-04-15 13:54:46 +0000

    - Update assemblies and installer to allow easy development upgrades

--- Liquesce-PhaseI/LiquesceSvc/Properties/AssemblyInfo.cs
+++ Liquesce-PhaseI/LiquesceSvc/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2012.1.27.867")]
-[assembly: AssemblyFileVersion("2012.1.27.867")]
+[assembly: AssemblyVersion("12.4.884.15")]
+[assembly: AssemblyFileVersion("12.4.884.15")]
