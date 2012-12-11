f4afe4aa971a7cb0fc84dcfde8948923668bff8b
smurfiv_cp <SND\smurfiv_cp>
2012-12-11 19:11:55 +0000

    - Mounting into a folder -- Need to create it as an access point in XP -- Check Drive exists -- Create folder if it does not exist -- Allow drag drop into the mount point - Start to investigate Mgt app start-up invokes. - Add "Service not running" text back in - Modify service exit state to capture when mounting is not successful and force service close with the appropriate error code.

--- Liquesce-PhaseI-NoSharing/Liquesce/Properties/AssemblyInfo.cs
+++ Liquesce-PhaseI-NoSharing/Liquesce/Properties/AssemblyInfo.cs
@@ -57,5 +57,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.11.941.23")]
-[assembly: AssemblyFileVersion("12.11.941.23")]
+[assembly: AssemblyVersion("12.12.953.11")]
+[assembly: AssemblyFileVersion("12.12.953.11")]
