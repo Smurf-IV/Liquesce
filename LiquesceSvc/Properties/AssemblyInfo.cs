25c5de3e6ff349dc3a0acce0705ca622cf799205
smurfiv_cp <SND\smurfiv_cp>
2013-11-28 20:45:02 +0000

    2013-11-## - Building VM's for Test (2k12 doesn't want to play !) - Raised a few issues from own usage. - Getting latest CBFS V4 to fix some issues with: - - Installation of the helper DLLs failed sporadically with error 1155. - - Memory leak when the NotifyDirectoryChange API was calling inside the CBFS callbacks. - - Possibility of access violation in the user mode part of CallbackFS in the case of some directory enumeration requests, such as "dir "*\"". - Trying to discover which is the best way to get minimum size and best install experience - Setting up 7-ZIP SFX Maker - Re-issue

--- LiquesceSvc/Properties/AssemblyInfo.cs
+++ LiquesceSvc/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.10.1544.22")]
-[assembly: AssemblyFileVersion("13.10.1544.22")]
+[assembly: AssemblyVersion("13.11.1546.28")]
+[assembly: AssemblyFileVersion("13.11.1546.28")]
